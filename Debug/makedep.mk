22b0ec376851d1a7dc060feaa1d6dd3ee4558bc7
lazzox <lazzo@Live.com>
2016-04-06 09:02:55 +0200

    5.APril.2016
    Novi kod za logiku

---
+++ Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc_driver.c
+
+hardware.c
+
+interrupt.c
+
+mechanism.c
+
+port_driver.c
+
+TC_driver.c
+
+usart_driver.c
+
+main.c
+
